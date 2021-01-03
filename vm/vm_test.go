1068f9f8d375bfb1b375147eda4f488c141163a2
Tom Basche <tcbasche@gmail.com>
2021-01-03 19:00:27 +1100

    Support if/else in the VM

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -47,6 +47,15 @@ func TestBooleanExpressions(t *testing.T) {
 	runVmTests(t, tests)
 }
 
+func TestConditionals(t *testing.T) {
+	tests := []vmTestCase{
+		{"if (true) { 10 }", 10},
+		{"if (true) { 10 } else { 20 }", 10},
+		{"if (false) { 10 } else { 20 }", 20},
+	}
+	runVmTests(t, tests)
+}
+
 func runVmTests(t *testing.T, tests []vmTestCase) {
 	t.Helper()
 
